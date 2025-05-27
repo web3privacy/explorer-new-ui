89b24f726304d40c91bc6202badf7581b28a9190
lucilapastore <lucilapastore@gmail.com>
2025-05-27 15:11:23 -0300

    feat: restructure Header component to add "add project" button

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,3 +1,5 @@
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
 import { MainNav } from "./MainNav";
 import { MobileNav } from "./MobileNav";
 import { SocialIcons } from "./SocialIcons";
@@ -8,9 +10,23 @@ export function Header() {
       <div className="container flex h-16 items-center justify-between px-4 md:px-6 lg:px-8">
         <MainNav />
 
-        <div className="flex items-center gap-4">
-          <SocialIcons className="hidden lg:flex" />
-          <MobileNav />
+        <div className="flex items-center gap-8">
+          <div>
+            <Link
+              href="https://explorer.web3privacy.info/project/create"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hidden lg:flex items-center gap-2 bg-primary text-primary-foreground hover:bg-primary/90 px-4 py-0 rounded-md text-sm font-medium transition-colors"
+            >
+              <CirclePlus className="h-4 w-4" />
+              <span>Add Project</span>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <SocialIcons className="hidden lg:flex" />
+            <MobileNav />
+          </div>
         </div>
       </div>
     </header>
