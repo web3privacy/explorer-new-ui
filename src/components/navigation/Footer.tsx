fff56e9ed9cc2cbe68546b3e0edaa1f00cf7a531
lucilapastore <lucilapastore@gmail.com>
2025-07-17 16:06:03 -0300

    feat: add new explorer logos and update footer image source

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -10,15 +10,15 @@ export function Footer() {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8 lg:gap-12">
             <div className="md:col-span-2">
               <Link
-                href="https://web3privacy.info/about/"
+                href="https://hackmd.io/@m-f-/HJZ3aZSekl"
                 className="flex items-center space-x-2"
               >
                 <Image
-                  src="/logo.svg"
+                  src="/explorer-logo1.png"
                   alt="Web3Privacy Now"
-                  width={177}
-                  height={48}
-                  className="h-8 w-auto"
+                  width={266}
+                  height={72}
+                  className="h-12 w-auto"
                   priority
                 />
               </Link>
